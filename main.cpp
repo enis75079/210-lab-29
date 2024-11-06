ba786956f12debd6b88c7627c0c79dde36a8c44b
Naveen Islam <naveenislam@Naveens-MacBook-Air.local>
2024-11-05 17:28:21 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ Naveen Islam
 // headers for file handling, data structures, strings, and more
 
 // function prototypes
-// function to simulate inventory management over the 12 month period
+// function to simulate inventory management over the 52 week period
     // paramenters: map of the clothing items, intervals for the months
 
 // define main function
@@ -18,3 +18,19 @@ Naveen Islam
     // read data from a file containing information on the items and input them to the map
         // for each line, the item will specify the clothing type (shirt, pants, or shoe) and the details of the item
         // inserts the item into the appropriate list in the array for its item
+
+    // closes the file
+    // begin a simulation for managing iventory
+        // 52 time intervals (per week)
+            //  iterate through each clothing item in the map
+                
+                // for each item, simulate weekly margin
+
+                // simulate customers purchasing items
+                    // determine how many items are sold for each item randomly
+                    // move items that are sold from their list (in stock) to out of stock
+                    // assign discounts based on customer purchasing habits...
+                    // update the revenue based on how many items are sold that week
+                
+                // restock simulation
+                    // check stock levels. If the item has reached below a certain threshold, then add a 
