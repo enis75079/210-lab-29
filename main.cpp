907d01b5778a5006d78853b732c08351ebd04cc8
Naveen Islam <naveenislam@Naveens-MacBook-Air.local>
2024-11-05 17:20:42 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -9,5 +9,12 @@ Naveen Islam
 // headers for file handling, data structures, strings, and more
 
 // function prototypes
+// function to simulate inventory management over the 12 month period
+    // paramenters: map of the clothing items, intervals for the months
 
 // define main function
+    // initialize a map that stores the different clothing items which are associated with an array of lists (in stock, discount, or out of stock)
+
+    // read data from a file containing information on the items and input them to the map
+        // for each line, the item will specify the clothing type (shirt, pants, or shoe) and the details of the item
+        // inserts the item into the appropriate list in the array for its item
