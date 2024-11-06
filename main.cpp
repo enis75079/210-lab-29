cd3852f949cb0ef64c79f51e0c6d4cb9d420ab1e
Naveen Islam <naveenislam@Naveens-MacBook-Air.local>
2024-11-05 17:16:40 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+
+lab29: Project Proposal, Pseudocode, & Mockup
+COMSC-210
+Naveen Islam
+
+*/
+
+// headers for file handling, data structures, strings, and more
+
+// function prototypes
+
+// define main function
